fe4bf67f3d7d31f7a70a9043c45edf20e1896f18
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-11-04 17:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const sounds = ["sound1", "sound2", "sound3", "sound4", "sound5", "sound6"];
-
 const buttonsContainer = document.getElementById("buttons");
 
 let currentAudio = null;
@@ -15,6 +14,7 @@ sounds.forEach(sound => {
       currentAudio.pause();
       currentAudio.currentTime = 0;
     }
+
     currentAudio = new Audio(`./sounds/${sound}.mp3`);
     currentAudio.play().catch(err => {
       console.warn("Audio play failed:", err);
